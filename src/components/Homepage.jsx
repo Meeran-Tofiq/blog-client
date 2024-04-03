20d06f8da3062c8bb698b194b8e9674fb34d5439
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-04-04 00:00:27 +0300

    Feat hero image in homepage

---
+++ src/components/Homepage.jsx
@@ -0,0 +1,11 @@
+import HeroImage from "./HeroImage";
+
+export default function Homepage({}) {
+	return (
+		<>
+			<HeroImage imageUrl={"/images/heroImage.jpg"}>
+				<h1>HEAR THE VOICE OF THE WORLD</h1>
+			</HeroImage>
+		</>
+	);
+}
