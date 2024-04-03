fe9bd8a77a169e2e44d9d2a1f37a30f3633caff3
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-04-03 23:30:20 +0300

    Feat header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+export default function Header({}) {
+	return (
+		<header>
+			<h1>BLOG</h1>
+			<nav>
+				<ul>
+					<li>
+						<a href="">Sign up</a>
+					</li>
+					<li>
+						<a href="">Log in</a>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	);
+}
