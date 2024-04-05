18e2a7cc353bab4aea768783201c77c2d53d2af4
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-04-05 23:48:28 +0300

    Feat blog card component to show on homepage

---
+++ src/components/BlogCard.jsx
@@ -0,0 +1,21 @@
+import { DateTime } from "luxon";
+
+export default function BlogCard({ blogPost }) {
+	const { title, author, dateString } = blogPost;
+	const date = new Date(dateString);
+	const dateFormatted = DateTime.fromJSDate(this.date).toLocaleString(
+		DateTime.DATETIME_MED
+	);
+
+	return (
+		<article>
+			<header>
+				<h2>{title}</h2>
+			</header>
+			<aside>
+				<h4>{author}</h4>
+				<h4>{dateFormatted}</h4>
+			</aside>
+		</article>
+	);
+}
