f9aa3b082edab6cdf3fbcdb011fd8c93f71457e2
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-04-15 17:54:04 +0300

    Feat route handlign for login form

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,24 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
 import Homepage from "./components/Homepage.jsx";
+import LoginForm from "./components/LoginForm.jsx";
 
 async function customFetch(url) {
 	const res = fetch(import.meta.env.VITE_API_URL + url);
+	return res;
+}
+
+async function postFormData(data, event, url) {
+	event.preventDefault();
+
+	const res = await fetch(import.meta.env.VITE_API_URL + url, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(data),
+	});
+
 	return res;
 }
 
@@ -13,6 +28,10 @@ const router = createBrowserRouter([
 	{
 		path: "/",
 		element: <Homepage fetch={customFetch} />,
+	},
+	{
+		path: "/login",
+		element: <LoginForm postFormData={postFormData} />,
 	},
 ]);
 
