adc976ae0cb8e496003a3d65e986c34f75b1f4ca
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-05-27 16:37:51 +0300

    Refactor to call BlogPost component, w/ isBlogCard

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -1,25 +1,5 @@
-import { DateTime } from "luxon";
+import BlogPost from "./BlogPost";
 
 export default function BlogCard({ blogPost }) {
-	const { title, user, content, date: dateString } = blogPost;
-	const author = user.username;
-	const date = new Date(dateString);
-	const dateFormatted = DateTime.fromJSDate(date).toLocaleString(
-		DateTime.DATETIME_MED
-	);
-
-	return (
-		<article>
-			<header>
-				<h2>{title}</h2>
-				<aside>
-					<h4>{author}</h4>
-					<h4>{dateFormatted}</h4>
-				</aside>
-			</header>
-			<main>
-				<p className="truncate">{content}</p>
-			</main>
-		</article>
-	);
+	return <BlogPost blogPost={blogPost} isBlogCard="true" />;
 }
