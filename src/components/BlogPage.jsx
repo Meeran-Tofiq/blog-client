a939c183d07763d08bce4677d9ff67d8c7c841a1
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-05-27 17:47:59 +0300

    Feat page that renders blog post and comments

---
+++ src/components/BlogPage.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import BlogComment from "./BlogComment";
+import BlogPost from "./BlogPost";
+import { useLocation } from "react-router-dom";
+
+export default function BlogPage({ fetch }) {
+	const location = useLocation();
+	const { blogPost } = location.state;
+	const [comments, setComments] = useState([]);
+
+	useEffect(() => {
+		const fetchBlogComments = async () => {
+			try {
+				const res = await fetch(`/blog-posts/${blogPost._id}/comments`);
+				if (!res.ok)
+					throw new Error("Failed to fetch comments for this post");
+
+				const json = await res.json();
+				setComments(json.data);
+			} catch (error) {
+				console.error("Error fetching blog posts: ", error);
+			}
+		};
+
+		fetchBlogComments();
+	}, []);
+
+	return (
+		<>
+			<BlogPost blogPost={blogPost} />
+
+			<ol className="blog-comments">
+				{comments.map((comment) => {
+					return (
+						<li key={comment._id}>
+							<BlogComment comment={comment} />
+						</li>
+					);
+				})}
+			</ol>
+		</>
+	);
+}
