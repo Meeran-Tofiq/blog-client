c9ecd1989d60b6fc2953fd275bc436c2e2fab65f
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-05-28 17:49:24 +0300

    Refactor all methods into one custom fetch

--- src/api/fetch.js
+++ src/api/fetch.js
@@ -1,87 +1,46 @@
 import { useContext } from "react";
 import AuthContext from "../context/AuthContext";
 
-function useGet() {
-	/**
-	 * Sends a get request to the specified url.
-	 *
-	 * @param {string} url - The url to send the request to.
-	 * @returns {Promise} The promise, which if resolved, gives the server response.
-	 */
-	async function get(url) {
-		const res = fetch(import.meta.env.VITE_API_URL + url);
-		return res;
-	}
-
-	return get;
-}
-
-function usePost() {
-	/**
-	 * Sends a post request to the specified url.
-	 *
-	 * @param {string} url - The url to send the request to.
-	 * @param {object} data - The data to be sent.
-	 * @returns {Promise} The promise, which if resolved, gives the server response.
-	 */
-	async function post(url, data) {
-		const res = await fetch(import.meta.env.VITE_API_URL + url, {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify(data),
-		});
-
-		return res;
-	}
-
-	return post;
-}
-
-function usePostWithAuth() {
+const useFetchWithAuth = () => {
 	const { token } = useContext(AuthContext);
 
 	/**
-	 * Fetches a resource with an Authorization header.
-	 * If there is no authorization token, it will throw an error.
+	 * Fetches a resource with an optional Authorization header if a token is available.
 	 *
-	 * @param {string} url - The url to send the request to.
-	 * @param {object} data - The data to be sent.
+	 * @param {string} url - The URL of the resource.
 	 * @param {Object} [options={}] - Additional fetch options.
+	 * @param {boolean} [requireAuth=false] - Whether the token is required.
 	 * @returns {Promise<Object>} The response data as a JSON object.
-	 * @throws Will throw an error if the request fails or if no token is available.
+	 * @throws Will throw an error if the request fails or if no token is available when required.
 	 */
-	async function postWithAuth(url, data, options = {}) {
+	const fetchWithAuth = async (url, options = {}, requireAuth = false) => {
 		const headers = options.headers || {};
-		// Check if token is available
-		if (!token) {
+
+		// Check if token is required but not available
+		if (requireAuth && !token) {
 			return Promise.reject(new Error("no auth token was available"));
 		}
 
-		const res = await fetch(import.meta.env.VITE_API_URL + url, {
+		if (token) {
+			headers["Authorization"] = `Bearer ${token}`;
+		}
+
+		const response = await fetch(import.meta.env.VITE_API_URL + url, {
 			...options,
 			headers: {
 				...headers,
 				"Content-Type": "application/json",
 			},
-			body: JSON.stringify(data),
 		});
 
-		return res;
-	}
+		if (!response.ok) {
+			throw new Error(`HTTP error! status: ${response.status}`);
+		}
 
-	return postWithAuth;
-}
-
-export default function useFetch() {
-	const get = useGet();
-	const post = usePost();
-	const postWithAuth = usePostWithAuth();
-
-	return {
-		get,
-		post,
-		postWithAuth,
+		return response;
 	};
-}
+
+	return fetchWithAuth;
+};
+
+export default useFetchWithAuth;
