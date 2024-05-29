7bb7683b46c186e896145e756c3ae39cea3aa55b
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-05-29 15:32:12 +0300

    Refactor to fit new BlogComment requirements

--- src/components/BlogPage.jsx
+++ src/components/BlogPage.jsx
@@ -4,28 +4,32 @@ import BlogPost from "./BlogPost";
 import { useLocation } from "react-router-dom";
 import useFetchWithAuth from "../api/fetch";
 
-export default function BlogPage({}) {
+export default function BlogPage() {
 	const location = useLocation();
 	const { blogPost } = location.state;
 	const [comments, setComments] = useState([]);
 	const fetch = useFetchWithAuth();
 
+	const fetchBlogComments = async () => {
+		try {
+			const res = await fetch(`/blog-posts/${blogPost._id}/comments`);
+			if (!res.ok)
+				throw new Error("Failed to fetch comments for this post");
+
+			const json = await res.json();
+			setComments(json.data);
+		} catch (error) {
+			console.error("Error fetching blog post comments: ", error);
+		}
+	};
+
 	useEffect(() => {
-		const fetchBlogComments = async () => {
-			try {
-				const res = await fetch(`/blog-posts/${blogPost._id}/comments`);
-				if (!res.ok)
-					throw new Error("Failed to fetch comments for this post");
-
-				const json = await res.json();
-				setComments(json.data);
-			} catch (error) {
-				console.error("Error fetching blog posts: ", error);
-			}
-		};
-
 		fetchBlogComments();
 	}, []);
+
+	const handleCommentUpdated = () => {
+		fetchBlogComments();
+	};
 
 	return (
 		<>
@@ -34,13 +38,14 @@ export default function BlogPage({}) {
 			<div className="blog-comments-container">
 				<h2>Comments</h2>
 				<ol className="blog-comments">
-					{comments.map((comment) => {
-						return (
-							<li key={comment._id}>
-								<BlogComment comment={comment} />
-							</li>
-						);
-					})}
+					{comments.map((comment) => (
+						<li key={comment._id}>
+							<BlogComment
+								comment={comment}
+								onCommentUpdated={handleCommentUpdated}
+							/>
+						</li>
+					))}
 				</ol>
 			</div>
 		</>
