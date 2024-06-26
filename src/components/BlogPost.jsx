11fbed56de757b1d2e0aaa94baf8924af41fe444
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-06-26 17:18:21 +0300

    Fix homepage post display including format syntax

--- src/components/BlogPost.jsx
+++ src/components/BlogPost.jsx
@@ -1,4 +1,5 @@
 import { DateTime } from "luxon";
+import ContentDisplay from "./ContentDisplay";
 
 export default function BlogPost({ blogPost, isBlogCard }) {
 	const { title, user, content, date: dateString } = blogPost;
@@ -11,14 +12,18 @@ export default function BlogPost({ blogPost, isBlogCard }) {
 	return (
 		<article className={isBlogCard ? "blog-card" : "blog-post"}>
 			<header className="blog-header">
-				<h2>{title}</h2>
+				<h2>
+					<ContentDisplay htmlContent={title} />
+				</h2>
 				<aside>
 					<h4>{author}</h4>
 					<h4>{dateFormatted}</h4>
 				</aside>
 			</header>
 			<main className="blog-content">
-				<p className={isBlogCard ? "truncate" : ""}>{content}</p>
+				<p className={isBlogCard ? "truncate" : ""}>
+					<ContentDisplay htmlContent={content} />
+				</p>
 			</main>
 		</article>
 	);
